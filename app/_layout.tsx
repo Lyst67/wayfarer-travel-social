687f2831b48beadcb14b934280f8fae013da2ce6
pavlo lystopad <p_listopad@ukr.net>
2024-11-28 17:56:09 +0200

    add_icon

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,13 +1,17 @@
 import { Stack } from "expo-router";
-import { StatusBar } from "expo-status-bar";
-import React from "react";
-
+import { setStatusBarStyle } from "expo-status-bar";
+import React, { useEffect } from "react";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 export default function RootLayout() {
+  useEffect(() => {
+    setTimeout(() => {
+      setStatusBarStyle("light");
+    }, 0);
+  }, []);
+
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
-      <StatusBar style="dark" />
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="+not-found" options={{}} />
